0df7976b40fbb223dd7f3c9d98bd83ec3429b3a8
marcivallario <68357009+marcivallario@users.noreply.github.com>
2022-03-08 16:20:28 -0500

    Passenger edit style complete

--- client/src/components/PassengerView.js
+++ client/src/components/PassengerView.js
@@ -146,23 +146,52 @@ function PassengerView({ user, passengers }) {
                 <Content>
                     <div id="passenger-edit">
                         <h1>Edit Passenger</h1>
-                        <form id="add-passenger-form" onSubmit={handleUpdate}>
+                        <form id="edit-passenger-form" onSubmit={handleUpdate}>
+                            <label className="edit-label" htmlFor="legal_first_name">Legal First Name:</label>
                             <input value={formData.legal_first_name} name="legal_first_name" onChange={handleChange}></input>
+
+                            <label className="edit-label" htmlFor="legal_last_name">Legal Last Name:</label>
                             <input placeholder="Legal Last Name" value={formData.legal_last_name} name="legal_last_name" onChange={handleChange}></input>
+
+                            <label className="edit-label" htmlFor="nickname">Nickname:</label>
                             <input placeholder="Nickname (Optional)" value={formData.nickname} name="nickname" onChange={handleChange}></input>
+
+                            <label className="edit-label" htmlFor="position">Position:</label>
                             <input placeholder="Position" value={formData.position} name="position" onChange={handleChange}></input>
+
+                            <label className="edit-label" htmlFor="department">Department:</label>
                             <input placeholder="Department" value={formData.department} name="department" onChange={handleChange}></input>
+
+                            <label className="edit-label" htmlFor="cell">Cell: </label>
                             <input placeholder="Cell" value={formData.cell} name="cell" onChange={handleChange}></input>
+
+                            <label className="edit-label" htmlFor="email">Email:</label>
                             <input placeholder="Email" value={formData.email} name="email" onChange={handleChange}></input>
+
+                            <label className="edit-label"  htmlFor="dob">Date of Birth:</label>
                             <input placeholder="Date of Birth" value={formData.dob} name="dob" onChange={handleChange}></input>
+
+                            <label className="edit-label"  htmlFor="country_of_residence">Country of Residence:</label>
                             <input placeholder="Country of Residence" value={formData.country_of_residence} name="country_of_residence" onChange={handleChange}></input>
+
+                            <label className="edit-label"  htmlFor="state_of_residence">State of Residence:</label>
                             <input placeholder="State of Residence (if USA)" value={formData.state_of_residence} name="state_of_residence" onChange={handleChange}></input>
+
+                            <label className="edit-label"  htmlFor="passport">Passport:</label>
                             <input placeholder="Passport" value={formData.passport} name="passport" onChange={handleChange}></input>
+
+                            <label className="edit-label"  htmlFor="license">License:</label>
                             <input placeholder="License Number" value={formData.license} name="license" onChange={handleChange}></input>
+
+                            <label className="edit-label"  htmlFor="tsa_precheck">TSA Precheck:</label>
                             <input placeholder="TSA Precheck (optional)" value={formData.tsa_precheck} name="tsa_precheck" onChange={handleChange}></input>
+
+                            <label className="edit-label"  htmlFor="global_entry">Global Entry:</label>
                             <input placeholder="Global Entry (optional)" value={formData.global_entry} name="global_entry" onChange={handleChange}></input>
+
+                            <label className="edit-label" htmlFor="seat_assignment_pref">Seat Assignment Preference:</label>
                             <input placeholder="Seat Assignment Preference" value={formData.seat_assignment_pref} name="seat_assignment_pref" onChange={handleChange}></input>
-                            <input type="submit" value="Save Changes" />
+                            <input className="passenger-edit" type="submit" value="Save Changes" />
                         </form>
                     </div>
                 </Content>
