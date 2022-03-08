014d30b8e7cf609106bd99d5edf2ad19aef06938
marcivallario <68357009+marcivallario@users.noreply.github.com>
2022-03-08 16:03:13 -0500

    Passenger view style complete

--- client/src/components/PassengerView.js
+++ client/src/components/PassengerView.js
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from 'react';
 import { Layout } from 'antd';
+import '../styles/passengerview.css'
 
 function PassengerView({ user, passengers }) {
     const params = useParams();
@@ -25,6 +26,7 @@ function PassengerView({ user, passengers }) {
         seat_assignment_pref: ''
     })
     const { Header, Footer, Content } = Layout;
+    const today = new Date();
 
     useEffect(() => {
         if (user.id && params.passengerId) {
@@ -86,30 +88,49 @@ function PassengerView({ user, passengers }) {
         })
     }
 
+    const formatDate = (dateString) => {
+        const options = { year: "numeric", month: "long", day: "numeric", hour: "numeric", minute: "numeric" }
+        return new Date(dateString).toLocaleTimeString(undefined, options)
+    }
+
     
     if (!toggleEdit && passenger.id) {
         return (
             <Layout className="site-layout">
                 <Header className="site-layout-background" style={{ padding: 0 }}>
                     <div id="dashboard-header-content">
-                        <p>March 11th, 2022 11:19 AM. Welcome, {user.first_name} {user.last_name}.</p>
+                        <p>{formatDate(today)}</p>
+                        <p>Welcome, {user.first_name} {user.last_name}.</p>
                     </div>
                 </Header>
                 <Content>
-                    <h1>Passenger: {passenger.legal_first_name} {passenger.nickname? `"${passenger.nickname}"` : <></>} {passenger.legal_last_name}</h1>
-                    <p>Position: {passenger.position}</p>
-                    <p>Department: {passenger.department}</p>
-                    <p>Cell: {passenger.cell}</p>
-                    <p>Email: {passenger.email}</p>
-                    <p>Date of Birth: {passenger.dob}</p>
-                    <p>Country of Residence: {passenger.country_of_residence}</p>
-                    {passenger.country_of_residence === 'USA'? <p>State of Residence: {passenger.state_of_residence}</p> : <></>}
-                    <p>Passport: {passenger.passport}</p>
-                    <p>License #: {passenger.license}</p>
-                    {passenger.tsa_precheck ? <p>TSA Precheck: {passenger.tsa_precheck}</p> : <></>}
-                    {passenger.global_entry ? <p>Global Entry: {passenger.global_entry}</p> : <></>}
-                    <p>Seat Assignment Preferences: {passenger.seat_assignment_pref}</p>
-                    <button onClick={toggleEditForm}>Edit</button>
+                    <div id="passenger-view">
+                        <h1>Passenger: {passenger.legal_first_name} {passenger.nickname? `"${passenger.nickname}"` : <></>} {passenger.legal_last_name}</h1>
+                        <div id="passenger-view-details">
+                            <p className="view-label">Position:</p>
+                            <p>{passenger.position}</p>
+                            <p className="view-label">Department:</p>
+                            <p>{passenger.department}</p>
+                            <p className="view-label">Cell:</p>
+                            <p>{passenger.cell}</p>
+                            <p className="view-label">Email:</p>
+                            <p>{passenger.email}</p>
+                            <p className="view-label">Date of Birth:</p>
+                            <p>{passenger.dob}</p>
+                            <p className="view-label">Country of Residence:</p>
+                            <p>{passenger.country_of_residence}</p>
+                            {passenger.country_of_residence === 'USA'? <><p className="view-label">State of Residence:</p> <p>{passenger.state_of_residence}</p></>: null}
+                            <p className="view-label">Passport:</p>
+                            <p>{passenger.passport}</p>
+                            <p className="view-label">License #</p>
+                            <p>{passenger.license}</p>
+                            {passenger.tsa_precheck ? <><p className="view-label">TSA Precheck:</p> <p>{passenger.tsa_precheck}</p></>: null}
+                            {passenger.global_entry ? <><p className="view-label">Global Entry:</p> <p>{passenger.global_entry}</p></>: null}
+                            <p className="view-label">Seat Assignment Preferences:</p>
+                            <p>{passenger.seat_assignment_pref}</p>
+                        </div>
+                        <button className="passenger-edit" onClick={toggleEditForm}>Edit</button>
+                    </div>
                 </Content>
                 <Footer style={{ textAlign: 'center' }}>TrekCheck © 2022. All Rights Reserved.</Footer>
             </Layout>
@@ -123,25 +144,27 @@ function PassengerView({ user, passengers }) {
                     </div>
                 </Header>
                 <Content>
-                    <h1>Edit Passenger</h1>
-                    <form id="add-passenger-form" onSubmit={handleUpdate}>
-                        <input value={formData.legal_first_name} name="legal_first_name" onChange={handleChange}></input>
-                        <input placeholder="Legal Last Name" value={formData.legal_last_name} name="legal_last_name" onChange={handleChange}></input>
-                        <input placeholder="Nickname (Optional)" value={formData.nickname} name="nickname" onChange={handleChange}></input>
-                        <input placeholder="Position" value={formData.position} name="position" onChange={handleChange}></input>
-                        <input placeholder="Department" value={formData.department} name="department" onChange={handleChange}></input>
-                        <input placeholder="Cell" value={formData.cell} name="cell" onChange={handleChange}></input>
-                        <input placeholder="Email" value={formData.email} name="email" onChange={handleChange}></input>
-                        <input placeholder="Date of Birth" value={formData.dob} name="dob" onChange={handleChange}></input>
-                        <input placeholder="Country of Residence" value={formData.country_of_residence} name="country_of_residence" onChange={handleChange}></input>
-                        <input placeholder="State of Residence (if USA)" value={formData.state_of_residence} name="state_of_residence" onChange={handleChange}></input>
-                        <input placeholder="Passport" value={formData.passport} name="passport" onChange={handleChange}></input>
-                        <input placeholder="License Number" value={formData.license} name="license" onChange={handleChange}></input>
-                        <input placeholder="TSA Precheck (optional)" value={formData.tsa_precheck} name="tsa_precheck" onChange={handleChange}></input>
-                        <input placeholder="Global Entry (optional)" value={formData.global_entry} name="global_entry" onChange={handleChange}></input>
-                        <input placeholder="Seat Assignment Preference" value={formData.seat_assignment_pref} name="seat_assignment_pref" onChange={handleChange}></input>
-                        <input type="submit" value="Save Changes" />
-                    </form>
+                    <div id="passenger-edit">
+                        <h1>Edit Passenger</h1>
+                        <form id="add-passenger-form" onSubmit={handleUpdate}>
+                            <input value={formData.legal_first_name} name="legal_first_name" onChange={handleChange}></input>
+                            <input placeholder="Legal Last Name" value={formData.legal_last_name} name="legal_last_name" onChange={handleChange}></input>
+                            <input placeholder="Nickname (Optional)" value={formData.nickname} name="nickname" onChange={handleChange}></input>
+                            <input placeholder="Position" value={formData.position} name="position" onChange={handleChange}></input>
+                            <input placeholder="Department" value={formData.department} name="department" onChange={handleChange}></input>
+                            <input placeholder="Cell" value={formData.cell} name="cell" onChange={handleChange}></input>
+                            <input placeholder="Email" value={formData.email} name="email" onChange={handleChange}></input>
+                            <input placeholder="Date of Birth" value={formData.dob} name="dob" onChange={handleChange}></input>
+                            <input placeholder="Country of Residence" value={formData.country_of_residence} name="country_of_residence" onChange={handleChange}></input>
+                            <input placeholder="State of Residence (if USA)" value={formData.state_of_residence} name="state_of_residence" onChange={handleChange}></input>
+                            <input placeholder="Passport" value={formData.passport} name="passport" onChange={handleChange}></input>
+                            <input placeholder="License Number" value={formData.license} name="license" onChange={handleChange}></input>
+                            <input placeholder="TSA Precheck (optional)" value={formData.tsa_precheck} name="tsa_precheck" onChange={handleChange}></input>
+                            <input placeholder="Global Entry (optional)" value={formData.global_entry} name="global_entry" onChange={handleChange}></input>
+                            <input placeholder="Seat Assignment Preference" value={formData.seat_assignment_pref} name="seat_assignment_pref" onChange={handleChange}></input>
+                            <input type="submit" value="Save Changes" />
+                        </form>
+                    </div>
                 </Content>
                 <Footer style={{ textAlign: 'center' }}>TrekCheck © 2022. All Rights Reserved.</Footer>
             </Layout>
