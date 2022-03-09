223944b8cb2dbb76a251a9b4cea28d152a1f9794
marcivallario <68357009+marcivallario@users.noreply.github.com>
2022-03-08 19:42:12 -0500

    Trip edit styles complete

--- client/src/components/TripEdit.js
+++ client/src/components/TripEdit.js
@@ -70,11 +70,18 @@ function TripEdit({ trips, trip, user, formatDate, onUpdateTrip, setTrip, setTri
             transportations.map(transpo => {
                 return (
                     <Card key={transpo.id} title={transpo.direction}>
-                        <p>Date: {formatDate(transpo.date)}</p>
-                        <p>Method: {transpo.trans_mode}</p>
-                        <p>{transpo.confirmation? `Confirmation #: ${transpo.confirmation}` : <></>}</p>
-                        <p>{transpo.notes? `Notes: ${transpo.notes}` : null }</p>
-                        <button type="button" onClick={() => {
+                        <div className="trip-detail-card">
+                            <p className="trip-view-label">Date:</p>
+                            <p>{formatDate(transpo.date)}</p>
+
+                            <p className="trip-view-label">Method:</p>
+                            <p>{transpo.trans_mode}</p>
+
+                            {transpo.confirmation? <><p className="trip-view-label">Confirmation #:</p><p>{transpo.confirmation}</p></>: null}
+
+                            {transpo.notes? <><p className="trip-view-label">Notes:</p><p>{transpo.notes}</p></>: null}
+                        </div>
+                        <button className="trip-edit" type="button" onClick={() => {
                             setToggleTranspoEdit(true)
                             setSelectedTranspo(transpo)
                             }}>Edit Transportation</button>
@@ -89,15 +96,31 @@ function TripEdit({ trips, trip, user, formatDate, onUpdateTrip, setTrip, setTri
             accommodations.map(acc=> {
                 return (
                     <Card  key={acc.id} title={acc.acc_type}>
-                        <p>{acc.name}</p>
-                        <p>{acc.address_1}</p>
-                        {acc.address_2? <p>{acc.address_2}</p> : <div></div>}
-                        <p>{acc.city}, {acc.state} {acc.zip}</p>
-                        <p></p>
-                        <p>Checkin: {formatDate(acc.checkin)}</p>
-                        <p>Checkout: {formatDate(acc.checkout)}</p>
-                        <p>Confirmation #{acc.confirmation}</p>
-                        <button type="button" onClick={() => {
+                        <div className="trip-detail-card">
+                            <p className="trip-view-label">Name: </p>
+                            <p>{acc.name}</p>
+                            
+                            <p className="trip-view-label">Address:</p>
+                            <div>
+                                <p>{acc.address_1}</p>
+                                {acc.address_2? <p>{acc.address_2}</p> : null}
+                                <p>{acc.city}, {acc.state} {acc.zip}</p>
+                            </div>
+                            
+
+                            <p className="trip-view-label">Phone: </p>
+                            <p>{acc.phone}</p>
+                            
+                            <p className="trip-view-label">Checkin:</p>
+                            <p>{formatDate(acc.checkin)}</p>
+
+                            <p className="trip-view-label">Checkout:</p>
+                            <p>{formatDate(acc.checkout)}</p>
+
+                            <p className="trip-view-label">Confirmation #:</p>
+                            <p>{acc.confirmation}</p>
+                        </div>
+                        <button className="trip-edit" type="button" onClick={() => {
                             setToggleAccEdit(true)
                             setSelectedAcc(acc)
                             }}>Edit Accommodation</button>
@@ -112,13 +135,24 @@ function TripEdit({ trips, trip, user, formatDate, onUpdateTrip, setTrip, setTri
             flights.map(flight => {
                 return (
                     <Card key={flight.id} title={flight.leg}>
-                        <p>Flight: {flight.airline} {flight.flight_no}</p>
-                        <p>Departs: {flight.dep_airport} @ {formatDate(flight.dep_time)}</p>
-                        <p>Arrives: {flight.arr_airport} @ {formatDate(flight.arr_time)}</p>
-                        <p>Seat: {flight.seat}</p>
-                        <p>Confirmation: {flight.confirmation}</p>
-                        {flight.notes? <p>Notes: {flight.notes}</p> : <></>}
-                        <button type="button" onClick={() => {
+                        <div className="trip-detail-card">
+                            <p className="trip-view-label">Flight:</p>
+                            <p>{flight.airline} {flight.flight_no}</p>
+
+                            <p className="trip-view-label">Departs:</p>
+                            <p>{flight.dep_airport} @ {formatDate(flight.dep_time)}</p>
+
+                            <p className="trip-view-label">Arrives:</p>
+                            <p>{flight.arr_airport} @ {formatDate(flight.arr_time)}</p>
+
+                            <p className="trip-view-label">Seat:</p>
+                            <p>{flight.seat}</p>
+
+                            <p className="trip-view-label">Confirmation:</p>
+                            <p> {flight.confirmation}</p>
+                            {flight.notes? <><p className="trip-view-label">Notes:</p><p>{flight.notes}</p> </>: null}
+                        </div>
+                        <button className="trip-edit" type="button" onClick={() => {
                             setToggleFlightEdit(true)
                             setSelectedFlight(flight)
                             }}>Edit Flight</button>
@@ -128,17 +162,29 @@ function TripEdit({ trips, trip, user, formatDate, onUpdateTrip, setTrip, setTri
         )
     }
 
+    const formatDateOnly = (dateString) => {
+        let d = new Date(dateString)
+        return (d.getMonth()+1)+'/'+d.getDate()+'/'+ d.getFullYear();
+    }
+
     return (
-        <div>
+        <div id="trip-edit">
             <h1>Edit Trip</h1>
-            <form id="edit-trip" onSubmit={handleUpdate}>
-                <input value={formData.depart} name="depart" onChange={handleChange}></input>
-                <input value={formData.return} name="return" onChange={handleChange}></input>
-                <label htmlFor="itinerary_sent">Itinerary sent?</label>
-                <input type="checkbox" id="itinerary_sent" name="itinerary_sent" checked={formData.itinerary_sent} onChange={handleCheckedChange}/>
+            <form id="edit-trip-form" onSubmit={handleUpdate}>
+                <div id="trip-edit-details">
+                    <label className="edit-label" htmlFor="depart">Depart: </label>
+                    <input value={formatDateOnly(formData.depart)} name="depart" onChange={handleChange}></input>
+
+                    <label className="edit-label" htmlFor="return">Return</label>
+                    <input value={formatDateOnly(formData.return)} name="return" onChange={handleChange}></input>
+
+                    <label className="edit-label" htmlFor="itinerary_sent">Itinerary sent?</label>
+                    <input type="checkbox" id="itinerary_sent" name="itinerary_sent" checked={formData.itinerary_sent} onChange={handleCheckedChange}/>
+                </div>
+
                 <Collapse defaultActiveKey={[`${trip.flights.length > 0 ? '1' : <></>}`, `${trip.transportations.length > 0 ? '2' : <></>}`, `${trip.accommodations.length > 0 ? '3' : <></>}`]}>
                     <Panel header="Flights" key="1">
-                    {trip.flights.length > 0? renderFlights(trip.flights) : <p>No flights on this trip!</p>/* <button>Add Flight</button>*/}
+                    {trip.flights.length > 0? renderFlights(trip.flights) : <p>No flights on this trip!</p>}
                     </Panel>
                     <Panel header="Transportation" key="2">
                     {trip.transportations.length > 0? renderTranspo(trip.transportations) : <p>No transportations on this trip!</p>}
@@ -147,7 +193,7 @@ function TripEdit({ trips, trip, user, formatDate, onUpdateTrip, setTrip, setTri
                     {trip.accommodations.length > 0? renderAccommodations(trip.accommodations) : <p>No accommodations on this trip!</p>}
                     </Panel>
                 </Collapse>
-                <input type="submit" value="Save Changes"/>
+                <input className="trip-edit"type="submit" value="Save Changes"/>
                 {!!Object.values(selectedFlight).length && <FlightEditModal toggleShow={toggleFlightEdit} flight={selectedFlight} trip={trip} setToggle={setToggleFlightEdit} setSelectedFlight={setSelectedFlight} onUpdateTrip={onUpdateTrip} setTrip={setTrip} trips={trips} setTrips={setTrips}/>}
 
                 {!!Object.values(selectedTranspo).length && <TranspoEditModal toggleShow={toggleTranspoEdit} transpo={selectedTranspo} trip={trip} setToggle={setToggleTranspoEdit} setSelectedTranspo={setSelectedTranspo} setTrip={setTrip} trips={trips} setTrips={setTrips}/>}
